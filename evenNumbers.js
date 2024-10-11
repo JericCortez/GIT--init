2e5e6f6a4670397393e0a7a04854c4242eccc9bf
JericCortez <jericayson2@gmail.com>
2024-10-11 10:41:48 +0000

    initial

---
+++ evenNumbers.js
@@ -0,0 +1,13 @@
+// Program to print numbers from 1 to 10
+console.log("Numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Modify the program to print only even numbers between 1 and 10
+console.log("Even numbers between 1 and 10:");
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
